632c34a4f02fe356875001e4c8a628fce9af70be
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-01-10 20:27:30 -0600

    Ring-Buffer: Append: Testing for less than capacity and doing a direct add

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -4,11 +4,19 @@ from doubly_linked_list import DoublyLinkedList
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
+        # current refers to the current item that needs to be removed or replaced
         self.current = None
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        #Check if the length is less than the capacity. If so it does not need any replacement.
+        # It just needs to be added to the double linked list
+        # Also if the length is the same as capacity then the first item pushed in will be the
+        # one to be replaced.
+        if self.storage.length < self.capacity:
+            self.storage.add_to_tail(item)
+            if (self.storage.length == self.capacity):
+                self.current = self.storage.head
 
     def get(self):
         # Note:  This is the only [] allowed
